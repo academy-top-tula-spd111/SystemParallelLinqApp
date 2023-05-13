42fe7cfb991fb5492d1d2baf67b0b83f6e732cf4
maxefimov <efimov.top.academy@gmail.com>
2023-05-13 17:49:35 +0300

    Добавьте файлы проекта.

---
+++ SystemParallelLinqApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+List<int> intList = new List<int>();
+for(int i = 0; i < 20; i++) intList.Add(i + 1);
+
+//var square = from item in intList.AsParallel()
+//             orderby item
+//             select item * item;
+
+//(from item in intList.AsParallel()
+//             orderby item
+//             select item * item
+//             ).ForAll(Console.WriteLine);
+
+//intList.AsParallel().Select(i => i * i).ForAll(Console.WriteLine);
+var square = intList.AsParallel().AsOrdered().Select(i => i * i);
+
+foreach (var item in square)
+    Console.WriteLine(item);
